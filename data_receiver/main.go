1f32bac8f461b36f0eb9c903f5df8b304d5afc7b
Mohammadmohebi33 <mohammad.rasoul.mohebi@gmail.com>
2024-11-19 20:13:32 +0330

    send obu data on kafka

--- data_receiver/main.go
+++ data_receiver/main.go
@@ -1,15 +1,20 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/Mohammadmohebi33/toll_calculator/types"
+	"github.com/confluentinc/confluent-kafka-go/v2/kafka"
 	"github.com/gorilla/websocket"
 	"log"
 	"net/http"
 )
 
+var kafkaTopic = "obudata"
+
 func main() {
-	recv := NewDataReceiver()
+
+	recv, _ := NewDataReceiver()
 	http.HandleFunc("/ws", recv.wsHandler)
 	http.ListenAndServe(":8080", nil)
 
@@ -18,12 +23,48 @@ func main() {
 type DataReceiver struct {
 	msg  chan types.OBUData
 	conn *websocket.Conn
+	prod *kafka.Producer
 }
 
-func NewDataReceiver() *DataReceiver {
-	return &DataReceiver{
-		msg: make(chan types.OBUData, 128),
+func NewDataReceiver() (*DataReceiver, error) {
+	p, err := kafka.NewProducer(&kafka.ConfigMap{"bootstrap.servers": "localhost"})
+	if err != nil {
+		return nil, err
 	}
+
+	// Delivery report handler for produced messages
+	go func() {
+		for e := range p.Events() {
+			switch ev := e.(type) {
+			case *kafka.Message:
+				if ev.TopicPartition.Error != nil {
+					fmt.Printf("Delivery failed: %v\n", ev.TopicPartition)
+				} else {
+					fmt.Printf("Delivered message to %v\n", ev.TopicPartition)
+				}
+			}
+		}
+	}()
+	return &DataReceiver{
+		msg:  make(chan types.OBUData, 128),
+		prod: p,
+	}, nil
+}
+
+func (dr *DataReceiver) produceData(data types.OBUData) error {
+	b, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+
+	// Produce messages to topic (asynchronously)
+	topic := kafkaTopic
+	err = dr.prod.Produce(&kafka.Message{
+		TopicPartition: kafka.TopicPartition{Topic: &topic, Partition: kafka.PartitionAny},
+		Value:          b,
+	}, nil)
+
+	return err
 }
 
 func (dr DataReceiver) wsHandler(w http.ResponseWriter, r *http.Request) {
@@ -49,7 +90,9 @@ func (dr DataReceiver) wsReceiverLoop() {
 			continue
 		}
 
-		fmt.Printf("received obu date from [%d] :: <lat %2f, long %2f >\n", data.OBUID, data.Lat, data.Long)
-		dr.msg <- data
+		fmt.Println("data receive :", data)
+		if err := dr.produceData(data); err != nil {
+			log.Println("produce error", err)
+		}
 	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/Mohammadmohebi33/toll_calculator
 
 go 1.23.0
 
-require github.com/gorilla/websocket v1.5.3 // indirect
+require (
+	github.com/confluentinc/confluent-kafka-go/v2 v2.6.1 // indirect
+	github.com/gorilla/websocket v1.5.3 // indirect
+)
